440b3955ed7d0f95c4bcffe474dbe6b5a136d832
Daniel YL YIP <2602505y@student.gla.ac.uk>
2022-04-14 12:49:37 +0100

    baisc route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
 const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,13 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+app.get('/', (req, res)=>{
+    res.send(`welcome`);
+})
 
 server.listen(PORT, ()=>{
     mongoose.connect(process.env.MONGODB_URL)
