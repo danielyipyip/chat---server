969896ebebddd0a732870ee00595c77195c63074
Daniel YL YIP <2602505y@student.gla.ac.uk>
2022-04-16 11:56:49 +0100

    alter model

--- model/MessageModel.js
+++ model/MessageModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const messageSchema = mongoose.Schema({
     sender: {type: String, required: true}, 
     message: {type: String, required: true}, 
-    createDate: {type: Date, default: new Date()}
+    createDate: {type: Date, default: new Date()},
+    room: {type: String, required: true} //in initial phase, input room num first
 })
 
 const messageModel = mongoose.model('message', messageSchema);
